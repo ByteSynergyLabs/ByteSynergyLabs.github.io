aad68db63f929409e14a88b2a299b5385244c5a3
Dthiwanka <dulthiwanka2015@gmail.com>
2025-03-13 17:13:31 +0530

    Initial Updated

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -7,7 +7,7 @@ jQuery(document).ready(function($) {
       ferror = false,
       emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
 
-    f.children('input').each(function() { // run all inputs
+    f.children('input').each(function() { // run all inputs updated
 
       var i = $(this); // current input
       var rule = i.attr('data-rule');
